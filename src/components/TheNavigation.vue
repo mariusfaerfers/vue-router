ba60f9d87168596ffaddc15b3d94a4ae4e10c2c4
Marius Färfers <M.Faerfers@verband.creditreform.de>
2025-11-12 15:58:23 +0100

    second commit

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">brazil</router-link>
+    <router-link to="/hawaii">hawaii</router-link>
+    <router-link to="/jamaica">jamaica</router-link>
+    <router-link to="/panama">panama</router-link>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,28 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/brazil",
+    name: "brazil",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/* webpackChunkName: "brazil" */ "@/views/BrazilView.vue"),
+  },
+  {
+    path: "/hawaii",
+    name: "hawaii",
+    component: () =>
+      import(/* webpackChunkName: "hawaii" */ "@/views/HawaiiView.vue"),
+  },
+  {
+    path: "/jamaica",
+    name: "jamaica",
+    component: () =>
+      import(/* webpackChunkName: "jamaica" */ "@/views/JamaicaView.vue"),
+  },
+  {
+    path: "/panama",
+    name: "panama",
+    component: () =>
+      import(/* webpackChunkName: "panama" */ "@/views/PanamaView.vue"),
   },
 ];
 
